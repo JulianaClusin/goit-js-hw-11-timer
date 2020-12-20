af1ff5e7266187694c23bcdf08b5ee6c00ef1010
Juliana Clusin <naymenko95@gmail.com>
2020-12-20 23:40:36 +0200

    Updates

--- src.f316fac5.js
+++ src.f316fac5.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/main.css");var e=r(require("./js/timer"));function r(e){return e&&e.__esModule?e:{default:e}}var t=new e.default({selector:"#timer-1",targetDate:new Date("Dec 31, 2020, 23:59:59")});
 },{"./css/main.css":"I7Ak","./js/timer":"yCUT"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-timer/src.3f2d5ffe.js.map
+//# sourceMappingURL=/goit-js-hw-11-timer/src.f316fac5.js.map
