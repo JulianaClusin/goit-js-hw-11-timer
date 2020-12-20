8d46d1ba006a46145603e18a1e46b6a2e140ea8b
Juliana Clusin <naymenko95@gmail.com>
2020-12-20 23:19:22 +0200

    add timer

--- src/index.js
+++ src/index.js
@@ -1,54 +1,8 @@
 import './css/main.css';
 
-const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Dec 31, 2020, 23:59:59'),
-  });
+import CountdownTimer from './js/timer';
 
-  export default class CountdownTimer {
-    constructor({ selector, targetDate }) {
-      this.selector = selector;
-      this.targetDate = targetDate;
-      this.days = document.querySelector(`${selector} [data-value="days"]`);
-      this.hours = document.querySelector(`${selector} [data-value="hours"]`);
-      this.minutes = document.querySelector(`${selector} [data-value="mins"]`);
-      this.seconds = document.querySelector(`${selector} [data-value="secs"]`);
-      this.startTimer();
-    }
-  
-    startTimer() {
-      this.getTime();
-      setInterval(() => {
-        this.getTime();
-      }, 1000);
-    }
-  
-    getTime() {
-      const currentTime = Date.now();
-      const deltaTime = this.targetDate - currentTime;
-      const time = this.getTimeComponents(deltaTime);
-      this.timerInterface(time);
-    }
-  
-    getTimeComponents(date) {
-      const days = this.pad(Math.floor(date / (1000 * 60 * 60 * 24)));
-      const hours = this.pad(
-        Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      );
-      const mins = this.pad(Math.floor((date % (1000 * 60 * 60)) / (1000 * 60)));
-      const secs = this.pad(Math.floor((date % (1000 * 60)) / 1000));
-  
-      return { days, hours, mins, secs };
-    }
-  
-    timerInterface(data) {
-      this.days.textContent = String(data.days);
-      this.hours.textContent = String(data.hours);
-      this.minutes.textContent = String(data.mins);
-      this.seconds.textContent = String(data.secs);
-    }
-  
-    pad(value) {
-      return String(value).padStart(2, '0');
-    }
-  }
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Dec 31, 2020, 23:59:59'),
+});
---
+++ src/js/timer.js
@@ -0,0 +1,47 @@
+export default class CountdownTimer {
+    constructor({ selector, targetDate }) {
+      this.selector = selector;
+      this.targetDate = targetDate;
+      this.days = document.querySelector(`${selector} [data-value="days"]`);
+      this.hours = document.querySelector(`${selector} [data-value="hours"]`);
+      this.minutes = document.querySelector(`${selector} [data-value="mins"]`);
+      this.seconds = document.querySelector(`${selector} [data-value="secs"]`);
+      this.startTimer();
+    }
+  
+    startTimer() {
+      this.getTime();
+      setInterval(() => {
+        this.getTime();
+      }, 1000);
+    }
+  
+    getTime() {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.timerInterface(time);
+    }
+  
+    getTimeComponents(date) {
+      const days = this.pad(Math.floor(date / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = this.pad(Math.floor((date % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = this.pad(Math.floor((date % (1000 * 60)) / 1000));
+  
+      return { days, hours, mins, secs };
+    }
+  
+    timerInterface(data) {
+      this.days.textContent = String(data.days);
+      this.hours.textContent = String(data.hours);
+      this.minutes.textContent = String(data.mins);
+      this.seconds.textContent = String(data.secs);
+    }
+  
+    pad(value) {
+      return String(value).padStart(2, '0');
+    }
+  }
